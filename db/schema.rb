ff20377ba9a0a2cfd65785f53e49cdbf6c776a0e
skogman <yim.apichai@gmail.com>
2014-07-23 00:02:51 +0700

    users can now sign up exclusively with github

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722102804) do
+ActiveRecord::Schema.define(version: 20140722164119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20140722102804) do
     t.string   "last_name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
